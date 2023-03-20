8f7f8945f2f8a38cb71509add48f4c5e8827953a
zhourenxiang <zhourenxiang@sensetime.com>
2023-03-20 23:54:39 +0800

    Init

---
+++ Assets/asoliddev - Auto Chess/Scripts/Buff/BuffBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//{ PerSecond, BeforeAttack, AfterAttack, BeforeHit, AfterHit }
+public class BuffBehaviour : MonoBehaviour
+{
+    public virtual void PerSecondActive()
+    {
+
+    }
+
+    public virtual void BeforeAttackActive()
+    {
+
+    }
+
+    public virtual void AfterAttackActive()
+    {
+
+    }
+
+    public virtual void BeforeHitActive()
+    {
+
+    }
+
+    public virtual void AfterHitActive()
+    {
+
+    }
+}
---
+++ Assets/asoliddev - Auto Chess/Scripts/Buff/Test/TestBuffBehaviour.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestBuffBehaviour : BuffBehaviour
+{
+    public override void AfterAttackActive()
+    {
+        base.AfterAttackActive();
+    }
+}
