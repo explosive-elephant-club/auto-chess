ca443e74cf6574bbdc833f29ae864e77980f80ee
helei <969147832@qq.com>
2025-02-23 21:22:32 +0800

    增加subviewBase，自动绑定工具修改

--- Assets/asoliddev - Auto Chess/Scripts/Test/BindTest.cs
+++ Assets/asoliddev - Auto Chess/Scripts/Test/BindTest.cs
@@ -9,19 +9,8 @@ public class BindTest : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
     }
-    #region 自动绑定
-    private Image _imgTest;
-    private Text _textTest;
-    #endregion
-    //自动获取组件添加字典管理
-    private void AutoBindingUI()
-    {
-        _imgTest = transform.Find("Test_Auto").GetComponent<Image>();
-        _textTest = transform.Find("Test_Auto").GetComponent<Text>();
-    }
-
+    
     // Update is called once per frame
     void Update()
     {
